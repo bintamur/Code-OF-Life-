c568abea1b8d9cb3ef11324c14ff3b15b4c78bc7
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-07-22 19:13:02 +0600

    Update URI 1010.c

--- URI 1010.c
+++ URI 1010.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d;
-    double e,f,bil;
-    scanf("%d%d%lf",&a,&b,&e);
-    scanf("%d%d%lf",&c,&d,&f);
-    bil=(b*e)+(d*f);
-    printf("VALOR A PAGAR: R$ %.2lf\n",bil);
+    int p1,p2,u1,u2;
+    double pr2,pr2,total;
+    scanf("%d%d%lf",&p1,&u1,&pr1);
+    scanf("%d%d%lf",&p2,&u2,&pr2);
+    total=(u1*pr1)+(u2*pr2);
+    printf("VALOR A PAGAR: R$ %.2lf\n",total);
     return 0;
 }
