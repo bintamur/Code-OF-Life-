1ef603bc444d7b274a59cb6415f17da775312ed5
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-07-27 11:05:33 +0600

    Update URI 1015.c

--- URI 1015.c
+++ URI 1015.c
@@ -7,7 +7,7 @@ double X1,Y1,X2,Y2,Distance;
 scanf("%lf%lf",&X1,&Y1);
 scanf("%lf%lf",&X2,&Y2);
 Distance=sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1)));
-printf("%.4lf",Distance);
+printf("%.4lf\n",Distance);
 return 0;
 
 
