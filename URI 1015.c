010101981524ab68618d4723485221ae320bd31f
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-07-27 10:54:18 +0600

    Add files via upload

---
+++ URI 1015.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+double X1,Y1,X2,Y2,Distance;
+scanf("%lf%lf",&X1,&Y1);
+scanf("%lf%lf",&X2,&Y2);
+Distance=sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1)));
+printf("%.4lf",Distance);
+return 0;
+
+
+}
