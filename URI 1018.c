07d498623aa109a263ce3b7e7311f053cf80b42a
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-01 21:06:10 +0600

    Update URI 1018.c

--- URI 1018.c
+++ URI 1018.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
-
+##include<stdio.h>
 int main()
 {
-
-  int a,t;
-  scanf("%d",&a);
-  printf("%d\n",a);
-  printf("%d nota(s) de R$ 100,00\n",a/100);
-  t = a%100;
-  printf("%d nota(s) de R$ 50,00\n",t/50);
-  t = t%50;
-  printf("%d nota(s) de R$ 20,00\n",t/20);
-   t = t%20;
-  printf("%d nota(s) de R$ 10,00\n",t/10);
-   t = t%10;
-  printf("%d nota(s) de R$ 5,00\n",t/5);
-   t = t%5;
-  printf("%d nota(s) de R$ 2,00\n",t/2);
-   t = t%2;
-  printf("%d nota(s) de R$ 1,00\n",t/1);
-  return 0;
+    int N;
+    scanf("%d",&N);
+    printf("%d\n",N);
+    printf("%d nota(s) de R$ 100,00\n",N/100);
+    N=N%100;
+    printf("%d nota(s) de R$ 50,00\n",N/50);
+    N=N%50;
+    printf("%d nota(s) de R$ 20,00\n",N/20);
+    N=N%20;
+    printf("%d nota(s) de R$ 10,00\n",N/10);
+    N=N%10;
+    printf("%d nota(s) de R$ 5,00\n",N/5);
+    N=N%5;
+    printf("%d nota(s) de R$ 2,00\n",N/2);
+    N=N%2;
+    printf("%d nota(s) de R$ 1,00\n",N/1);
+    return 0;
 }
