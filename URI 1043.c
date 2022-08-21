464f7d184c6f60d9291b227929a4b73df35dc4f8
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-22 00:24:39 +0600

    Update URI 1043.c

--- URI 1043.c
+++ URI 1043.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
 
+    float a, b, c, t, p;
+
+    scanf("%f%f%f", &a, &b, &c);
+
+    if (a+b > c && b+c > a && a+c > b)
+        printf("Perimetro = %.1f\n", a+b+c);
+
+    else
+        printf("Area = %.1f\n", ((a+b)*c)/2);
+
+    return 0;
+}
