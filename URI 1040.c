d6152e15837c3a980101acf7d9cb571257dd7a41
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-24 18:57:18 +0600

    Update URI 1040.c

--- URI 1040.c
+++ URI 1040.c
@@ -4,31 +4,38 @@ int main()
     double  a,b,c,d,e,k,sum;
     scanf("%lf%lf%lf%lf",&a,&b,&c,&d);
     k=(a*2+b*3+c*4+d*1)/10;
-    printf("Media: %.1lf\n",k);
-    if(k>=7.0)
-    {
-        printf("Aluno aprovado.\n");
-    }
-    else if(k<5.0)
-    {
-      printf("Aluno reprovado.\n");
-    }
-    else if(k>=5.0 && k<=6.9)
-    {
-       printf("Aluno em exame.\n");
-       scanf("%lf",&e);
-       printf("Nota do exame: %.1lf\n",e);
-       sum=(k+e)/2;
-       if(sum>=5.0)
-       {
-        printf("Aluno aprovado.\n");
-        }
-else if (sum<=4.9)
-{
-    printf("Aluno reprovado.\n");
-}
-printf("Media final: %.1lf",sum);
 
+    if(k<5)
+    {
+        printf("Media: %.1lf\n", k);
+        printf("Aluno reprovado.\n");
     }
-return 0;
+
+    if(k>=5.0 && k<=6.9)
+    {
+        scanf("%lf",&e);
+        printf("Media: %.1lf\n", k);
+        printf("Aluno em exame.\n");
+
+        printf("Nota do exame: %.1lf\n",e);
+        sum=(k+e)/2;
+
+        if(sum>=5.0)
+        {
+            printf("Aluno aprovado.\n");
+        }
+        if (sum<=4.9)
+        {
+            printf("Aluno reprovado.\n");
+        }
+        printf("Media final: %.1lf\n",sum);
+    }
+    else if(k>=7.0)
+    {
+       printf("Media: %.1lf\n", k);
+        printf("Aluno aprovado.\n");
+    }
+
+    return 0;
 }
+
